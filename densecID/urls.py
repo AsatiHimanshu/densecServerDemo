c5187c4d0dd4865f40b6cf286d21af2cda448553
AsatiHimanshu <82979224+AsatiHimanshu@users.noreply.github.com>
2022-04-11 05:06:45 -0700

    Update urls.py

--- densecID/urls.py
+++ densecID/urls.py
@@ -5,10 +5,10 @@ from . import views
 from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns=[
-
+#   url path to retreive the stored data for captured image  
     re_path(r'^post1/',csrf_exempt(views.post1), name='post1'),
-
+#   url path to add the data for captured image
     re_path(r'^post2/',csrf_exempt(views.post2), name='post2'),
-
+#   url path to update teh data for captured image
     re_path(r'^post3/',csrf_exempt(views.post3), name='post3')
 ]
